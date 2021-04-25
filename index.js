e209c83d64d903bb09bf1e35fd8ea5478aa5824d
yichuantai <ilgt33351@gmail.com>
2021-04-25 10:58:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const linebot = require('./lib/linebot');
 const express = require('express');
+const rp = require('request-promise');
+const bodyParser = require('body-parser');
+
+
+// 讀取開放資料庫資料
+function readAQI(repos){
+    let data;
+    for (i in repos) {
+        if (repos[i].SiteName == SITE_NAME) {
+            data = repos[i];
+            break;
+        }
+    }
+    return data;
+}
 
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
@@ -7,23 +22,61 @@ const bot = linebot({
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-const app = express();
-
 const linebotParser = bot.parser();
 
+const app = express();
+app.set('view engine', 'ejs');
+
+const SITE_NAME = '楠梓';
+
+// 空氣品質開放資料庫
+const aqiOpt = {
+    uri: "http://opendata2.epa.gov.tw/AQI.json",
+    json: true
+}; 
+
 app.get('/',function(req,res){
-    res.send('Hello World!');
+    rp(aqiOpt)
+    .then(function (repos) {
+        res.render('index', {AQI:readAQI(repos)});
+    })
+    .catch(function (err) {
+		res.send("無法取得空氣品質資料～");
+    });
 });
+
+
+//app.get('/',function(req,res){
+//    res.send('Hello World!');
+//});
 
 app.post('/linewebhook', linebotParser);
 
 // 回覆收到的訊息文字
 bot.on('message', function (event) {
-	event.reply(event.message.text).then(function (data) {
-		console.log('Success', data);
-	}).catch(function (error) {
-		console.log('Error', error);
-	});
+	switch (event.message.type) {
+		case 'text':
+			let data;
+			rp(aqiOpt)
+			.then(function (repos) {
+				data = readAQI(repos);
+				event.reply(
+					data.County + data.SiteName +'\n\nPM2.5指數：'+ data["PM2.5_AVG"] + 
+					    '\n狀態：' + data.Status);
+					})
+					.catch(function (err) {
+				event.reply('無法取得空氣品質資料～');
+					});
+			break;
+		case 'sticker':
+			event.reply({
+				type: 'sticker',
+				packageId: 1,
+				stickerId: 1
+			});
+			break;
+	}
+	
 });
 
 app.listen(process.env.PORT || 80, function () {
